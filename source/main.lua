14e6ccf8ef4ad4f56b6b187604212130251f8a72
EmryFairgrieve <emryfairgrieve@gmail.com>
2023-05-13 22:48:28 +0100

    Added background and cleaned up comments

--- source/main.lua
+++ source/main.lua
@@ -8,68 +8,32 @@ import "Player"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
------
-
 function init()
   local playerUpImage = gfx.image.new("images/playerUp")
   local playerDownImage = gfx.image.new("images/playerDown")
   local playerLeftImage = gfx.image.new("images/playerLeft")
   local playerRightImage = gfx.image.new("images/playerRight")
+  assert(playerUpImage)
+  assert(playerDownImage)
+  assert(playerLeftImage)
+  assert(playerRightImage)
 
   player = Player(playerUpImage, playerDownImage, playerLeftImage, playerRightImage)
 
-  --[[
-  -- Load images
-  
-  local playerImage = gfx.image.new("images/playerImage")
-  assert(playerImage) -- make sure the image was where we thought
-
   local backgroundImage = gfx.image.new("images/background")
   assert(backgroundImage)
-
-  -- Create player sprite 
-
-  playerSprite = gfx.sprite.new( playerImage )
-  playerSprite:moveTo( 200, 120 ) 
-  playerSprite:add()
-
-  -- Register draw background callback
 
   drawBackgroundCallback = function( x, y, width, height )
     backgroundImage:draw( 0, 0 )
   end
   gfx.sprite.setBackgroundDrawingCallback(drawBackgroundCallback)
-  --]]
-
-  -- MyClass = class('PeePee').extends(Object)
 end
 
 init()
 
 function playdate.update()
-  --[[
-  if playdate.buttonIsPressed( playdate.kButtonUp ) then
-    playerSprite:moveBy( 0, -2 )
-  end
-
-  if playdate.buttonIsPressed( playdate.kButtonRight ) then
-    playerSprite:moveBy( 2, 0 )
-  end
-
-  if playdate.buttonIsPressed( playdate.kButtonDown ) then
-      playerSprite:moveBy( 0, 2 )
-  end
-
-  if playdate.buttonIsPressed( playdate.kButtonLeft ) then
-      playerSprite:moveBy( -2, 0 )
-  end
-  --]]
-
   player:update()
-
-  -- Update sprite
   gfx.sprite.update()
 
-  -- Update time
-  playdate.timer.updateTimers()
+  pd.timer.updateTimers()
 end
